cba69bba9192e9a1c24f682efcbf0855d925ebf0
epdsant <epdsant@gmail.com>
2024-05-31 09:27:49 +0100

    Created and project setup folders

---
+++ Airbnb/App/AirbnbApp.swift
@@ -0,0 +1,18 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by EDSON SANTOS on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
